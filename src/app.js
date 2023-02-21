6dc27c18e2b4b36946b3c9b0b9dd76ff99b4a1a6
Amy <amykim4229@gmail.com>
2023-02-21 23:03:08 +0900

    #1 컴포넌트의 조건문, 반복문렌더링
    컴포넌트를 렌더링 할 때
    중간에 보간식을 이용해 배열을 반복하면서 필터링 혹은 map을 통한 가공 후 출력할 수 있다.

--- src/app.js
+++ src/app.js
@@ -4,27 +4,28 @@ export default class App extends Component {
   constructor() {
     super({
       state: {
-        inputText: ''
+        fruits: [
+          { name: 'Apple', price: 1000 },
+          { name: 'Cherry', price: 2000 },
+          { name: 'Strawberry', price: 3000 },
+          { name: 'Watermelon', price: 4000 },
+        ]
       }
     });
   }
 
   render() {
-    this.el.classList.add('search');
-    this.el.innerHTML = /*html*/`
-      <input />
-      <button>Click!</button>
-    `;
-
-    const inputEl = this.el.querySelector('input');
-    inputEl.addEventListener("input", () => {
-      this.state.inputText = inputEl.value;
-    });
-
-    const buttonEl = this.el.querySelector('button');
-    buttonEl.addEventListener("click", () => {
-      console.log(this.state.inputText);
-    });
+    console.log(this.state.fruits);
+    this.el.innerHTML =/* html */ `
+      <h1>Fruits</h1>
+      <ul>
+        ${this.state.fruits
+        .filter(fruit => fruit.price < 4000)
+        .map(fruit => `<li> ${fruit.name}  \t ${fruit.price}</li>`)
+        .join('')
+      }
+      </ul>
+  `
   }
 
 }
