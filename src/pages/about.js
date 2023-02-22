5f4c8efb50e6ac7b8ffaea2133e6e481397e6318
Amy <amykim4229@gmail.com>
2023-02-22 13:35:56 +0900

    #3 해쉬라우터
    페이지정보인 라우트를 작성하고 url에서 해쉬정보를 추출해 페이지를 이동하는 라우터 기능을 만들었다.
    
    스타일을 적용해 페이지 이동 시 스크롤이 top-left로 초기화 되도록 하였다.
    
    url의 경로정보와 쿼리스트링을 분리하고 쿼리스트링을 가공하였다.

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import Component from '../core/component.js';
+
+export default class About extends Component {
+
+  render() {
+    this.el.innerHTML = /*html*/ `
+      <h1>About !!!!!!!</h1>
+    `
+  }
+}
